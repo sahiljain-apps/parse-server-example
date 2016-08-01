4db56c64cea942e207474b90db24ce904f134aea
sahiljain-apps <jainsahil2001@gmail.com>
2016-08-01 14:56:36 -0700

    cloud code func

--- cloud/main.js
+++ cloud/main.js
@@ -1,4 +1,18 @@
+Parse.Cloud.afterSave("Post", function(request) {
+  // Our "Message" class has a "text" key with the body of the message itself
+  var messageText = request.object.get('songName');
 
-Parse.Cloud.define('hello', function(req, res) {
-  res.success('Hi');
+  var pushQuery = new Parse.Query(Parse.Installation);
+  pushQuery.equalTo('deviceType', 'ios'); // targeting iOS devices only
+
+  Parse.Push.send({
+    where: pushQuery, // Set our Installation query
+    data: {
+      alert: "songName: " + messageText
+    }
+  }).then(function() {
+    // Push was successful
+  }, function(error) {
+    throw "Got an error " + error.code + " : " + error.message;
+  });
 });
