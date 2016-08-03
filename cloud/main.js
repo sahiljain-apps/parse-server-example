8d3ec86ac7b0b39d91d3200a10e416c2d2665635
sahiljain-apps <jainsahil2001@gmail.com>
2016-08-03 10:11:10 -0700

    testing

--- cloud/main.js
+++ cloud/main.js
@@ -1,6 +1,14 @@
 Parse.Cloud.afterSave("Post", function(request) {
    query = new Parse.Query(Parse.Installation);
 
+   var followingQuery = Parse.Query("Follow");
+   followingQuery.where("toUser", request.object.get("fromUser"));
+
+  //  var userQuery = Parse.Query(Parse.User);
+  //  userQuery.matchesQuery(self, )
+  //
+  //  query.matchesQuery("fromUser", followingQuery);
+
    Parse.Push.send({
      where: query,
      data: { alert: '@ ' +  request.object.get("userName") + ' shared: ' + request.object.get("songName") + ' by: ' + request.object.get("songArtist") }
