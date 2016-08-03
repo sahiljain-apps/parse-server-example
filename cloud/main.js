bd3325b5542769e88d98e7c99f022ff2fb37c495
sahiljain-apps <jainsahil2001@gmail.com>
2016-08-03 10:58:19 -0700

    added channels

--- cloud/main.js
+++ cloud/main.js
@@ -1,16 +1,9 @@
 Parse.Cloud.afterSave("Post", function(request) {
-   query = new Parse.Query(Parse.Installation);
-   //
-  //  var followingQuery = Parse.Query("Follow");
-  //  followingQuery.where("toUser", request.object.get("fromUser"));
 
-  //  var userQuery = Parse.Query(Parse.User);
-  //  userQuery.matchesQuery(self, )
-  //
-  //  query.matchesQuery("fromUser", followingQuery);
+
 
    Parse.Push.send({
-     where: query,
+     channels: [request.object.objectId],
      data: { alert: '@ ' +  request.object.get("userName") + ' shared: ' + request.object.get("songName") + ' by: ' + request.object.get("songArtist") }
    }, { useMasterKey: true })
    .then(function() {
